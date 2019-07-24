01478bf56bece08bf5ff36750ec9dfcccc1d1817
Ryan Chang <rcchang.cw@gmail.com>
2019-07-25 00:54:40 +0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var db = req.db;
+    var collection = db.get('itemcollection');
+    collection.find({},{},function(e,docs){
+    	res.render('index', { "itemlist" : docs});
+    });
+});
+
+/* GET add page. */
+router.get('/add', function(req, res) {
+    var db = req.db;
+    var collection = db.get('itemcollection');
+    collection.find({},{},function(e,docs){
+    	res.render('add', { "itemlist" : docs});
+    });
+});
+
+/* GET specific item page */
+router.get('/item/:id', function(req, res, next) {
+    var db = req.db;
+    var itemid = req.params.id;
+    var collection = db.get('itemcollection');
+    collection.findOne({_id:itemid},{},function(e,doc){
+    	console.log(doc);
+    	res.render('item', { "item" : doc});
+    });
+});
+
+/* POST to Add Item */
+router.post('/additem', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var itemTitle = req.body.name;
+    var itemText = req.body.description;
+    var itemPrice = req.body.price;
+    var itemCondi = req.body.condition;
+    var itemContact = req.body.contact;
+    var itemLong = req.body.longitude;
+    var itemLat = req.body.latitude;
+    var itemImage = req.body.image;
+
+    // Set our collection
+    var collection = db.get('itemcollection');
+
+    // Submit to the DB
+    collection.insert({
+        "title" : itemTitle,
+        "text" : itemText,
+        "price" : itemPrice,
+        "condition" : itemCondi,
+        "contact" : itemContact,
+        "longitude" : itemLong,
+        "latitude" : itemLat,
+        "image" : itemImage
+
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("/");
+        }
+    });
+
+});
+module.exports = router;
